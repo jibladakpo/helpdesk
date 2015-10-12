1f61e64d8761f9cccaf6d47c514103582b5e5af1
jibladakpo <ladakpo.jibril@gmail.com>
2015-10-12 10:56:48 +0200

    firstest

---
+++ tests/firstTest.php
@@ -0,0 +1,11 @@
+<?php
+class firstTest extends _DefaultController
+{
+    public function setUp() {
+        parent::setUp();
+    }
+
+    public function testCanDoUnitTest() {
+        $this->assertTrue(true);
+    }
+}
