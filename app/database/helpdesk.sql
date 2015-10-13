976b9f11a8ea28f7c944182f8a334d7f6ed4dee9
jibladakpo <ladakpo.jibril@gmail.com>
2015-10-13 15:31:14 +0200

    SQL

--- app/database/helpdesk.sql
+++ app/database/helpdesk.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.3.11
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 14 Mai 2015 à 20:26
--- Version du serveur :  5.6.21
--- Version de PHP :  5.6.3
+-- Généré le :  Mar 13 Octobre 2015 à 15:18
+-- Version du serveur :  5.6.24
+-- Version de PHP :  5.6.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,8 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `helpdesk`
 --
-CREATE DATABASE IF NOT EXISTS `helpdesk` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `helpdesk`;
 
 -- --------------------------------------------------------
 
@@ -28,18 +26,17 @@ USE `helpdesk`;
 -- Structure de la table `categorie`
 --
 
-DROP TABLE IF EXISTS `Categorie`;
-CREATE TABLE IF NOT EXISTS `Categorie` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `categorie` (
+  `id` int(11) NOT NULL,
   `libelle` varchar(100) NOT NULL,
   `idCategorie` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `categorie`
 --
 
-INSERT INTO `Categorie` (`id`, `libelle`, `idCategorie`) VALUES
+INSERT INTO `categorie` (`id`, `libelle`, `idCategorie`) VALUES
 (1, 'Réseau', NULL),
 (2, 'Routage', 1),
 (3, 'Serveurs', 1),
@@ -56,9 +53,8 @@ INSERT INTO `Categorie` (`id`, `libelle`, `idCategorie`) VALUES
 -- Structure de la table `faq`
 --
 
-DROP TABLE IF EXISTS `Faq`;
-CREATE TABLE IF NOT EXISTS `Faq` (
-`id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `faq` (
+  `id` int(11) NOT NULL,
   `titre` varchar(100) NOT NULL,
   `contenu` text NOT NULL,
   `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -66,16 +62,18 @@ CREATE TABLE IF NOT EXISTS `Faq` (
   `idUser` int(11) NOT NULL,
   `version` varchar(20) NOT NULL DEFAULT '1.0',
   `popularity` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `faq`
 --
 
-INSERT INTO `Faq` (`id`, `titre`, `contenu`, `dateCreation`, `idCategorie`, `idUser`, `version`, `popularity`) VALUES
-(1, 'Comment formuler une demande à partir de l''interface web ?\r\n', 'L''adresse de l''application est https://...\r\n\r\nL''accès à l''interface web du HelpDesk requiert votre connexion sur le service d''authentification.  Une fois connecté, vous retrouverez tout ce qui concerne votre demande : groupe attributaire, technicien chargé du traitement, suivis, solution, …\r\n\r\nPour formuler une nouvelle demande, vous devez commencer par utiliser l''entrée du menu ou l''icône associée à « Créer un ticket ».\r\n\r\nDans le formulaire qui vous est alors proposé, seuls trois champs sont obligatoires :\r\n\r\nCatégorie : elle permet l''attribution de votre demande au groupe concerné ;\r\nTitre : il est utilisé pour afficher l''ensemble des demandes, aussi veillez à ce qu''il soit concis, à la fois synthétique et précis\r\nDescription : faites figurer ici toutes les informations que vous jugerez utiles pour permettre le traitement de votre demande.  Des informations complémentaires pourront vous être demandées si nécessaire.\r\nLes autres champs, bien qu''optionnels, peuvent être utiles voire nécessaires au traitement de votre demande :\r\n\r\nType : Incident (par défaut) en cas de dysfonctionnement, ou simple demande\r\nUrgence : Elle est croisée avec l''impact évalué par le technicien pour définir la priorité utilisée pour trier l''ensemble des demandes.\r\nSuivi par courriel : Choisissez « Non » si vous ne souhaitez pas recevoir d''information concernant votre demande par mél.\r\nÉlement associé : Si nécessaire et s''il est automatiquement identifié, vous pourrez ici associer votre poste de travail ou l''un de ses logiciels à votre demande.\r\nFichier : À utiliser pour joindre un fichier à votre demande.\r\nUne fois le formulaire rempli, créer votre ticket en cliquant sur le bouton « Envoyer message ».  Un compte-rendu apparaît alors dans lequel figure le numéro associé à votre demande – le ticket – que vous pourrez utiliser pour obtenir des informations a posteriori.', '2015-05-10 17:43:47', 11, 1, '1.0', 0),
+INSERT INTO `faq` (`id`, `titre`, `contenu`, `dateCreation`, `idCategorie`, `idUser`, `version`, `popularity`) VALUES
+(1, 'Comment formuler ', '<p>L&#39;adresse de l&#39;application est https://... L&#39;acc&egrave;s &agrave; l&#39;interface web du HelpDesk requiert votre connexion sur le service d&#39;authentification. Une fois connect&eacute;, vous retrouverez tout ce qui concerne votre demande : groupe attributaire, technicien charg&eacute; du traitement, suivis, solution, &hellip; Pour formuler une nouvelle demande, vous devez commencer par utiliser l&#39;entr&eacute;e du menu ou l&#39;ic&ocirc;ne associ&eacute;e &agrave; &laquo; Cr&eacute;er un ticket &raquo;. Dans le formulaire qui vous est alors propos&eacute;, seuls trois champs sont obligatoires : Cat&eacute;gorie : elle permet l&#39;attribution de votre demande au groupe concern&eacute; ; Titre : il est utilis&eacute; pour afficher l&#39;ensemble des demandes, aussi veillez &agrave; ce qu&#39;il soit concis, &agrave; la fois synth&eacute;tique et pr&eacute;cis Description : faites figurer ici toutes les informations que vous jugerez utiles pour permettre le traitement de votre demande. Des informations compl&eacute;mentaires pourront vous &ecirc;tre demand&eacute;es si n&eacute;cessaire. Les autres champs, bien qu&#39;optionnels, peuvent &ecirc;tre utiles voire n&eacute;cessaires au traitement de votre demande : Type : Incident (par d&eacute;faut) en cas de dysfonctionnement, ou simple demande Urgence : Elle est crois&eacute;e avec l&#39;impact &eacute;valu&eacute; par le technicien pour d&eacute;finir la priorit&eacute; utilis&eacute;e pour trier l&#39;ensemble des demandes. Suivi par courriel : Choisissez &laquo; Non &raquo; si vous ne souhaitez pas recevoir d&#39;information concernant votre demande par m&eacute;l. &Eacute;lement associ&eacute; : Si n&eacute;cessaire et s&#39;il est automatiquement identifi&eacute;, vous pourrez ici associer votre poste de travail ou l&#39;un de ses logiciels &agrave; votre demande. Fichier : &Agrave; utiliser pour joindre un fichier &agrave; votre demande. Une fois le formulaire rempli, cr&eacute;er votre ticket en cliquant sur le bouton &laquo; Envoyer message &raquo;. Un compte-rendu appara&icirc;t alors dans lequel figure le num&eacute;ro associ&eacute; &agrave; votre demande &ndash; le ticket &ndash; que vous pourrez utiliser pour obtenir des informations a posteriori.</p>\r\n', '2015-09-28 06:56:46', 11, 1, '1.0', 0),
 (2, 'À quoi sert le HelpDesk ?\r\n', 'Le HelpDesk correspond au projet 2 « Évolution de l''outil d''assistance » du programme 6 « Accompagner la consolidation et la transformation de la fonction SI au sein de notre établissement ».\r\n\r\nL''un des objectifs stratégiques à l''origine du projet est d''homogénéiser la prestation d''assistance sur tous les sites et pour tous les usagers afin d''offrir un niveau de service équitablement accessible.\r\n\r\nEn termes opérationnels, l''outil développé permet de disposer d’un guichet d’assistance unique, de mettre en œuvre des outils et des procédures communes et d''identifier les problèmes redondants.\r\n\r\nDu point de vue de l''usager, il apporte l''assurance d''un enregistrement formel des demandes et des fonctionnalités d''information et de suivi systématiques.', '2015-05-14 10:43:57', 11, 1, '1.0', 5),
-(3, 'Procédure de changement de mot de passe', '<h2>Objet</h2>\r\n\r\nCette procédure a pour but de fournir des conseils et des recommandations pour la création d''un mot de passe fort.\r\n\r\n<h2>Domaine d''application</h2>\r\n\r\nCette procédure s''adresse à tous les utilisateurs disposant d''un compte d''accès au système d''information\r\n\r\n<h2>Descriptif</h2>\r\n\r\n<h3>Pré-requis :</h3>\r\n\r\nUn bon mot de passe est un mot de passe suffisamment long, facile à retenir et très difficile à deviner. Votre mot de passse doit être constitué d''au moins 8 caractères dont une majuscule et un chiffre. Il peut contenir des lettres non accentuées, des chiffres, et certains caractères spéciaux : _ ! @ # $ % - + = < > ( ) { } [ ] | : ; , . ? ~ &\r\n\r\n<h3>Quelques procédés ou comment faire ?</h3>\r\n<ul>\r\n<li>Accoler mots et chiffres : Faire3Pas</li>\r\n<li>Créer un rébus : 71fame3MAIC&O (c''est un fameux 3 mâts Hisse et Ho)</li>\r\n<li>Pensez à une chanson ou un poème et extrayez les premières lettres : ottoc4ocR! (one, two, three, o''clock, four o''clock, rock !)</li>\r\n<li>Choisissez un mot de passe en y insérant des caractères spéciaux g1M2p#DUtI1 (j''ai un mot de passe différent du tien)</li>\r\n<li>Ne pas utiliser de mot de passe ayant un rapport avec soi (noms, dates de naissance,..)</li>\r\n<li>Vous avez tout intérêt à mélanger les possibilités offertes : lettres, chiffres et caractères spéciaux.</li>\r\n</ul>\r\n<h3>Respectez les règles</h3>\r\n\r\nVous êtes responsable de l''usage qui est fait de votre compte d''accès au système d''information. Pour garantir la sécurité de votre mot de passe, nous vous invitons à suivre les conseils ci-dessous:\r\n<ul>\r\n<li>Ne le communiquez à personne (il garantit votre identité et vous identifie personnellement dans notre système d''information</li>\r\n<li>Ne le notez pas sur un post-it</li>\r\n<li>Verrouillez ou fermez systématiquement votre session en quittant votre poste de travail</li>\r\n<li>Changez-le régulièrement</li>\r\n<li>N''utilisez pas le mot de passe de votre compte d''accès au système d''information pour un autre compte</li>\r\n</ul>', '2015-05-14 09:36:17', 12, 1, '1.0', 22);
+(3, 'Procédure de changement de mot de passe', '<h2>Objet</h2>\r\n\r\nCette procédure a pour but de fournir des conseils et des recommandations pour la création d''un mot de passe fort.\r\n\r\n<h2>Domaine d''application</h2>\r\n\r\nCette procédure s''adresse à tous les utilisateurs disposant d''un compte d''accès au système d''information\r\n\r\n<h2>Descriptif</h2>\r\n\r\n<h3>Pré-requis :</h3>\r\n\r\nUn bon mot de passe est un mot de passe suffisamment long, facile à retenir et très difficile à deviner. Votre mot de passse doit être constitué d''au moins 8 caractères dont une majuscule et un chiffre. Il peut contenir des lettres non accentuées, des chiffres, et certains caractères spéciaux : _ ! @ # $ % - + = < > ( ) { } [ ] | : ; , . ? ~ &\r\n\r\n<h3>Quelques procédés ou comment faire ?</h3>\r\n<ul>\r\n<li>Accoler mots et chiffres : Faire3Pas</li>\r\n<li>Créer un rébus : 71fame3MAIC&O (c''est un fameux 3 mâts Hisse et Ho)</li>\r\n<li>Pensez à une chanson ou un poème et extrayez les premières lettres : ottoc4ocR! (one, two, three, o''clock, four o''clock, rock !)</li>\r\n<li>Choisissez un mot de passe en y insérant des caractères spéciaux g1M2p#DUtI1 (j''ai un mot de passe différent du tien)</li>\r\n<li>Ne pas utiliser de mot de passe ayant un rapport avec soi (noms, dates de naissance,..)</li>\r\n<li>Vous avez tout intérêt à mélanger les possibilités offertes : lettres, chiffres et caractères spéciaux.</li>\r\n</ul>\r\n<h3>Respectez les règles</h3>\r\n\r\nVous êtes responsable de l''usage qui est fait de votre compte d''accès au système d''information. Pour garantir la sécurité de votre mot de passe, nous vous invitons à suivre les conseils ci-dessous:\r\n<ul>\r\n<li>Ne le communiquez à personne (il garantit votre identité et vous identifie personnellement dans notre système d''information</li>\r\n<li>Ne le notez pas sur un post-it</li>\r\n<li>Verrouillez ou fermez systématiquement votre session en quittant votre poste de travail</li>\r\n<li>Changez-le régulièrement</li>\r\n<li>N''utilisez pas le mot de passe de votre compte d''accès au système d''information pour un autre compte</li>\r\n</ul>', '2015-05-14 09:36:17', 12, 1, '1.0', 22),
+(4, 'question ', 'test', '2015-09-22 12:33:45', 2, 2, '1.0', 0),
+(5, 'test', 'testtt', '2015-09-22 12:39:35', 2, 1, '1.0', 0);
 
 -- --------------------------------------------------------
 
@@ -83,14 +81,13 @@ INSERT INTO `Faq` (`id`, `titre`, `contenu`, `dateCreation`, `idCategorie`, `idU
 -- Structure de la table `message`
 --
 
-DROP TABLE IF EXISTS `Message`;
 CREATE TABLE IF NOT EXISTS `Message` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `contenu` text NOT NULL,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `idUser` int(11) NOT NULL,
   `idTicket` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `message`
@@ -98,7 +95,11 @@ CREATE TABLE IF NOT EXISTS `Message` (
 
 INSERT INTO `Message` (`id`, `contenu`, `date`, `idUser`, `idTicket`) VALUES
 (1, 'Reçu<br>\r\nPouvez-vous préciser le message affiché ?<br>\r\nMerci', '2015-05-10 22:55:31', 1, 1),
-(2, 'Le message est <strong>`vidage de la mémoire physique...`</strong>', '2015-05-10 23:20:30', 2, 1);
+(2, 'Le message est <strong>`vidage de la mémoire physique...`</strong>', '2015-05-10 23:20:30', 2, 1),
+(3, '<p>test</p>\r\n', '2015-10-01 07:37:37', 1, 1),
+(5, '', '2015-10-01 07:38:54', 1, 2),
+(6, '<p>test123</p>\r\n', '2015-10-01 07:43:34', 2, 2),
+(7, '<p>testttttt</p>\r\n', '2015-10-01 07:46:20', 2, 1);
 
 -- --------------------------------------------------------
 
@@ -106,13 +107,12 @@ INSERT INTO `Message` (`id`, `contenu`, `date`, `idUser`, `idTicket`) VALUES
 -- Structure de la table `statut`
 --
 
-DROP TABLE IF EXISTS `Statut`;
 CREATE TABLE IF NOT EXISTS `Statut` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `libelle` varchar(30) NOT NULL,
   `ordre` int(11) NOT NULL,
   `icon` varchar(20) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `statut`
@@ -131,25 +131,26 @@ INSERT INTO `Statut` (`id`, `libelle`, `ordre`, `icon`) VALUES
 -- Structure de la table `ticket`
 --
 
-DROP TABLE IF EXISTS `Ticket`;
 CREATE TABLE IF NOT EXISTS `Ticket` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `titre` varchar(100) NOT NULL,
   `type` set('demande','incident') NOT NULL DEFAULT 'demande',
   `idCategorie` int(11) NOT NULL,
-  `titre` varchar(100) NOT NULL,
   `description` text NOT NULL,
-  `idStatut` int(11) NOT NULL,
   `idUser` int(11) NOT NULL,
+  `idStatut` int(11) NOT NULL,
   `dateCreation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `ticket`
 --
 
-INSERT INTO `Ticket` (`id`, `type`, `idCategorie`, `titre`, `description`, `idStatut`, `idUser`, `dateCreation`) VALUES
-(1, 'incident', 8, 'Ecran bleu', 'Ecran bleu sur ouverture session windows', 2, 2, '2015-05-10 16:27:29'),
-(2, 'incident', 12, 'impossible de se connecter', 'Impossible de se connecter à mon compte :\r\nLe message affiché est "Les informations de compte n''ont pas permis votre authentification".\r\n\r\nJe n''ai pas trouvé la procédure de récupération de mot de passe.', 1, 3, '2015-05-14 10:40:40');
+INSERT INTO `Ticket` (`id`, `titre`, `type`, `idCategorie`, `description`, `idUser`, `idStatut`, `dateCreation`) VALUES
+(1, 'Ecran bleu', '', 8, '<p>Ecran bleu sur ouverture session windows</p>\r\n', 2, 3, '2015-05-10 16:27:29'),
+(2, 'impossible de se connecter', 'incident', 12, 'Impossible de se connecter à mon compte :\r\nLe message affiché est "Les informations de compte n''ont pas permis votre authentification".\r\n\r\nJe n''ai pas trouvé la procédure de récupération de mot de passe.', 3, 1, '2015-05-14 10:40:40'),
+(3, 'logiciel défectueux', '', 9, '<p>Le logiciel ne marche pas. 2</p>\r\n', 2, 1, '2015-09-17 09:38:13'),
+(4, 'réseau', '', 8, '<p>marche pas&nbsp;</p>\r\n', 1, 1, '2015-09-28 08:40:33');
 
 -- --------------------------------------------------------
 
@@ -157,14 +158,13 @@ INSERT INTO `Ticket` (`id`, `type`, `idCategorie`, `titre`, `description`, `idSt
 -- Structure de la table `user`
 --
 
-DROP TABLE IF EXISTS `User`;
 CREATE TABLE IF NOT EXISTS `User` (
-`id` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `login` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
   `mail` varchar(255) NOT NULL,
   `admin` tinyint(1) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
 --
 -- Contenu de la table `user`
@@ -184,37 +184,37 @@ INSERT INTO `User` (`id`, `login`, `password`, `mail`, `admin`) VALUES
 -- Index pour la table `categorie`
 --
 ALTER TABLE `Categorie`
- ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`);
+  ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`);
 
 --
 -- Index pour la table `faq`
 --
 ALTER TABLE `Faq`
- ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`,`idUser`), ADD KEY `idUser` (`idUser`);
+  ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`,`idUser`), ADD KEY `idUser` (`idUser`);
 
 --
 -- Index pour la table `message`
 --
 ALTER TABLE `Message`
- ADD PRIMARY KEY (`id`), ADD KEY `idUser` (`idUser`), ADD KEY `idTicket` (`idTicket`);
+  ADD PRIMARY KEY (`id`), ADD KEY `idUser` (`idUser`), ADD KEY `idTicket` (`idTicket`);
 
 --
 -- Index pour la table `statut`
 --
 ALTER TABLE `Statut`
- ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `ticket`
 --
 ALTER TABLE `Ticket`
- ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`), ADD KEY `idStatut` (`idStatut`,`idUser`), ADD KEY `idUser` (`idUser`);
+  ADD PRIMARY KEY (`id`), ADD KEY `idCategorie` (`idCategorie`), ADD KEY `idStatut` (`idStatut`,`idUser`), ADD KEY `idUser` (`idUser`);
 
 --
 -- Index pour la table `user`
 --
 ALTER TABLE `User`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login` (`login`);
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `login` (`login`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -224,32 +224,32 @@ ALTER TABLE `User`
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `Categorie`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=13;
 --
 -- AUTO_INCREMENT pour la table `faq`
 --
 ALTER TABLE `Faq`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `message`
 --
 ALTER TABLE `Message`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT pour la table `statut`
 --
 ALTER TABLE `Statut`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `ticket`
 --
 ALTER TABLE `Ticket`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `User`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- Contraintes pour les tables exportées
 --
