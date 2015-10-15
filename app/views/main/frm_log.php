17428a890ba19d82a2d32e0fd675318d67ef3d29
Aboudou Rachidi <aboud976@live.fr>
2015-10-15 11:58:21 +0200

    connect1.2

--- app/views/main/frm_log.php
+++ app/views/main/frm_log.php
@@ -3,13 +3,13 @@
     <div class="form-group">
       <label class="control-label col-sm-2" for="email">Email:</label>
       <div class="col-sm-10">
-        <input type="email" class="form-control" id="email" placeholder="Enter email">
+        <input type="email" class="form-control" id="email" placeholder="Votre email">
       </div>
     </div>
     <div class="form-group">
       <label class="control-label col-sm-2" for="pwd">Password:</label>
       <div class="col-sm-10">          
-        <input type="password" class="form-control" id="pwd" placeholder="Enter password">
+        <input type="password" class="form-control" id="pwd" placeholder="Votre password">
       </div>
     </div>
     <div class="form-group">        
@@ -21,7 +21,7 @@
     </div>
     <div class="form-group">        
       <div class="col-sm-offset-2 col-sm-10">
-        <button type="submit" class="btn btn-default">Submit</button>
+        <input type="submit" value="Connexion" class="btn btn-default">
       </div>
     </div>
   </form>
